06b570f8b163bfab882722efe83989fa5268fd8d
tensho1026 <tensho1026@icloud.com>
2025-04-03 23:40:34 +0900

    DBからUserIDとUserScoreをとってくる関数を作った!

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import { playRandomNote } from "@/features/playRandomNote";
+import { getUserScore } from "@/features/getScore";
+import { getDbUserId } from "@/features/getUserId";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [userId, setUserId] = useState<string | null>(null);
+  const [userScore, setUserScore] = useState<number | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const score = await getUserScore();
+      setUserScore(score);
+    })();
+  }, []);
+
   return (
-    <div className=''>
-      <button onClick={playRandomNote}>Play Random Note</button>
+    <div>
+      <h1>ユーザースコア: {userScore ?? "取得中..."}</h1>
     </div>
   );
 }
