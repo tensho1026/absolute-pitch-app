651d9aed66dc03f6a97e91193463b4b1090fb0bb
tensho1026 <tensho1026@icloud.com>
2025-04-04 16:40:24 +0900

    feature/全ユーザーとスコアを取得する関数

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,23 +1,18 @@
-"use client";
+import { getAllUserScores } from "@/features/getAllScore";
 
-import { getUserScore } from "@/features/getScore";
-import { getDbUserId } from "@/features/getUserId";
-import { useEffect, useState } from "react";
-
-export default function Page() {
-  const [userId, setUserId] = useState<string | null>(null);
-  const [userScore, setUserScore] = useState<number | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const score = await getUserScore();
-      setUserScore(score);
-    })();
-  }, []);
+export default async function Page() {
+  const allScores = await getAllUserScores();
 
   return (
-    <div>
-      <h1>ユーザースコア: {userScore ?? "取得中..."}</h1>
+    <div className='p-4'>
+      <h1 className='text-xl font-bold mb-4'>全ユーザーのスコア</h1>
+      <ul className='list-disc pl-5 space-y-2'>
+        {allScores.map((user: any) => (
+          <li key={user.username}>
+            {user.username} : {user.score} 点
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/features/getAllScore.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { supabase } from "@/supabase/supabase.config";
+
+export async function getAllUserScores() {
+  const { data, error } = await supabase.from("User").select("username, score");
+
+  if (error) {
+    console.error("Supabase取得エラー:", error.message);
+    return [];
+  }
+
+  return data;
+}
